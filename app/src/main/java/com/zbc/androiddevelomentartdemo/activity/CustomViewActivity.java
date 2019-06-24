30f5e840b083a0ae42d8f63f28084f23d797e072
benchegnzhou <1107472286@qq.com>
2019-06-24 17:52:37 +0800

    首次提交

--- app/src/main/java/com/zbc/androiddevelomentartdemo/activity/CustomViewActivity.java
+++ app/src/main/java/com/zbc/androiddevelomentartdemo/activity/CustomViewActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
 
 import com.zbc.androiddevelomentartdemo.R;
+import com.zbc.androiddevelomentartdemo.view.CustomTextView;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
@@ -23,6 +24,8 @@ public class CustomViewActivity extends AppCompatActivity {
     TextView tvMessage;
     @Bind(R.id.tv_android_custom_view)
     TextView tvAndroidCustomView;
+    @Bind(R.id.tv_custom)
+    CustomTextView tvCustom;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,6 +42,9 @@ public class CustomViewActivity extends AppCompatActivity {
 
     private void initData() {
         tvMessage.setText("自定义控件基础知识介绍,一个控件自定义，先后经历view的测量、布局位置摆放、绘制");
+        tvCustom.setText("自定义控件基础知识介绍,一个控件自定义，先后经历view的测量、布局位置摆放、绘制")
+        .settextColor(0xffffffff)
+        .setTextSize(40);
     }
 
 
--- app/src/main/java/com/zbc/androiddevelomentartdemo/view/CustomTextView.java
+++ app/src/main/java/com/zbc/androiddevelomentartdemo/view/CustomTextView.java
@@ -1,13 +1,14 @@
 package com.zbc.androiddevelomentartdemo.view;
 
 import android.content.Context;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Paint;
+import android.graphics.Rect;
 import android.support.annotation.Nullable;
+import android.text.TextUtils;
 import android.util.AttributeSet;
 import android.view.View;
+import android.view.ViewTreeObserver;
 
 /**
  * Created by benchengzhou on 2019/6/16  15:59 .
@@ -17,11 +18,15 @@ import android.view.View;
  * 备    注：
  */
 
-public class CustomTextView extends View {
+public class CustomTextView extends View implements ViewTreeObserver.OnPreDrawListener {
     private int defaultWidth = 100;
     private int defaultHeight = 100;
     private String mText = "";
     private int mTextColor = 0xff666666;
+    private int mTextSize = 18;
+    private int mWidth;
+    private int mHeight;
+    private int mLineSpacing = 6;
 
 
     public CustomTextView(Context context) {
@@ -46,50 +51,82 @@ public class CustomTextView extends View {
     @Override
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
         //super.onMeasure(widthMeasureSpec, heightMeasureSpec);
-        int width = onWidthMeassure(widthMeasureSpec);
-        int height = onHeightMeassure(heightMeasureSpec);
-        setMeasuredDimension(width, height);
+        onWidthMeassure(widthMeasureSpec);
+        onHeightMeassure(heightMeasureSpec);
+        setMeasuredDimension(mWidth, mHeight);
     }
 
     private int onHeightMeassure(int heightMeasureSpec) {
-        int height = 0;
+        mHeight = 0;
         int mode = MeasureSpec.getMode(heightMeasureSpec);
         int size = MeasureSpec.getSize(heightMeasureSpec);
         switch (mode) {
             case MeasureSpec.EXACTLY:
-                height = size;
+                mHeight = size;
                 break;
             case MeasureSpec.AT_MOST:
-                height = Math.min(defaultHeight, size);
+//                height = Math.min(defaultHeight, size);
+                //获取文本的和padding的宽高作为控件的宽高
+                int paddingTop = getPaddingTop();
+                int paddingBottom = getPaddingBottom();
+                mHeight = getTextHeight(paddingTop, paddingBottom);
+
                 break;
             //对象可以无限的大，不受限制
             case MeasureSpec.UNSPECIFIED:
-                height = Math.min(defaultHeight, size);
+                mHeight = Math.min(defaultHeight, size);
                 break;
             default:
         }
-        return height;
+        return mHeight;
 
     }
 
+    /**
+     * 获取控件的高度
+     */
+    private int getTextHeight(int paddingTop, int paddingBottom) {
+        int height = 0;
+        Paint paint = new Paint();
+        paint.setAntiAlias(true);
+        paint.setColor(mTextColor);
+        paint.setTextSize(mTextSize);
+        Rect rect = new Rect();
+        paint.getTextBounds(mText, 0, mText.length() - 1, rect);
+
+        //判断文字的行数
+        if (!TextUtils.isEmpty(mText)) {
+            //超过一行
+            if (mWidth < rect.width()) {
+                int lineCount = rect.width() / mWidth + (rect.width() % mWidth == 0 ? 0 : 1);
+                height = lineCount * rect.height() + ((lineCount > 1 ? 0 : lineCount - 1) * mLineSpacing);
+            } else {
+                height = rect.height();
+            }
+        }
+        return height + paddingTop + paddingBottom;
+    }
+
     private int onWidthMeassure(int widthMeasureSpec) {
-        int width = 0;
+        mWidth = 0;
         int mode = MeasureSpec.getMode(widthMeasureSpec);
         int size = MeasureSpec.getSize(widthMeasureSpec);
         switch (mode) {
             case MeasureSpec.EXACTLY:
-                width = size;
+                mWidth = size;
                 break;
             case MeasureSpec.AT_MOST:
-                width = Math.min(defaultWidth, size);
+                int paddingLeft = getPaddingLeft();
+                int paddingRight = getPaddingRight();
+                mWidth = Math.min(defaultWidth, size);
                 break;
             //对象可以无限的大，不受限制
             case MeasureSpec.UNSPECIFIED:
-                width = Math.min(defaultWidth, size);
+                mWidth = Math.min(defaultWidth, size);
                 break;
             default:
         }
-        return width;
+        return mWidth;
     }
 
 
@@ -103,15 +140,83 @@ public class CustomTextView extends View {
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
         //创建bitmap用于报错bitmap图像,后续的绘图信息可以从里面直接取出并恢复
-        Bitmap bitmap = Bitmap.createBitmap(getWidth(), getHeight(), null);
-        canvas.drawBitmap(bitmap, 0, 0, null);
+//        Bitmap bitmap = Bitmap.createBitmap(getWidth(), getHeight(), null);
+//        canvas.drawBitmap(bitmap, 0, 0, null);
         Paint paint = new Paint();
-        paint.
+        paint.setAntiAlias(true);
+        paint.setColor(mTextColor);
+        paint.setTextSize(mTextSize);
+        Rect rect = new Rect();
+        paint.getTextBounds(mText, 0, mText.length() - 1, rect);
+        canvas.drawText(mText, 0 /*getPaddingLeft()*/, rect.height(), paint);
+    }
 
 
+    /**
+     * 设置文本
+     *
+     * @param text
+     */
+    public CustomTextView setText(CharSequence text) {
+        if (text == null) {
+            mText = "";
+        } else {
+            mText = text.toString();
+        }
+        invalidate();
+        return this;
+    }
 
-        canvas.drawText(mText, getPaddingLeft(), getPaddingTop(), );
+    /**
+     * 设置文本
+     *
+     * @param text
+     */
+    public CustomTextView setText(String text) {
+        if (text == null) {
+            mText = "";
+        } else {
+            mText = text;
+        }
+        requestLayout();
+        return this;
+    }
 
 
+    /**
+     * 设置文本的颜色
+     *
+     * @param textColor
+     */
+    public CustomTextView settextColor(int textColor) {
+        mTextColor = textColor;
+        requestLayout();
+        return this;
+    }
+
+
+    /**
+     * 实现textView数据刷新
+     */
+    public CustomTextView invalidateTextView() {
+        requestLayout();
+        return this;
+    }
+
+
+    /**
+     * 设置文本的大小
+     *
+     * @param textSize
+     * @return
+     */
+    public CustomTextView setTextSize(int textSize) {
+        mTextSize = textSize < 0 ? 0 : textSize;
+        return this;
+    }
+
+    @Override
+    public boolean onPreDraw() {
+        return false;
     }
 }
