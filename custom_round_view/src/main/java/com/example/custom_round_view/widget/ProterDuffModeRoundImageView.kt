d8dd68b136e9a36db2942ab603655e07d5f015fc
zhoubencheng <zhoubencheng@xiaomi.com>
2021-11-23 21:00:23 +0800

    tijao

--- custom_round_view/src/main/java/com/example/custom_round_view/widget/ProterDuffModeRoundImageView.kt
+++ custom_round_view/src/main/java/com/example/custom_round_view/widget/ProterDuffModeRoundImageView.kt
@@ -12,10 +12,18 @@ import com.example.custom_round_view.R
  */
 class ProterDuffModeRoundImageView(
     context: Context?,
-    @Nullable attrs: AttributeSet,
+    @Nullable attrs: AttributeSet?,
     defStyleAttr: Int
 ) :
     View(context, attrs, defStyleAttr) {
+
+
+    constructor(
+        context: Context?,
+        @Nullable attrs: AttributeSet?
+    ) : this(context, attrs, 0)
+
+    constructor(context: Context?) : this(context, null, 0)
 
 
     val mPaint by lazy {
@@ -25,19 +33,57 @@ class ProterDuffModeRoundImageView(
     }
 
 
+    //https://www.jianshu.com/p/d11892bbe055
     override fun draw(canvas: Canvas?) {
         super.draw(canvas)
+        val srcBitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888)
+        val destBitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888)
+//        createBitmap.eraseColor(0)
+        mPaint.color = Color.parseColor("#ffE64A19")
+//        mPaint.isAntiAlias = true                       //设置画笔为无锯齿
+        mPaint.setStrokeWidth(15f);//设置画笔粗细
+        mPaint.style = Paint.Style.FILL                 //实心矩形框
 
-        val decodeResource =
-            BitmapFactory.decodeResource(context.resources, R.drawable.ic_launcher_background)
+        Canvas(srcBitmap).drawRoundRect(
+            RectF(
+                50f,
+                50f,
+                srcBitmap.width.toFloat(),
+                srcBitmap.height.toFloat()
+            ), 30f, 30f, mPaint
+        )
+
+        mPaint.color = Color.parseColor("#ff1E88E5")
+        Canvas(destBitmap).drawRect(
+            0f,
+            0f,
+            destBitmap.width.toFloat(),
+            destBitmap.height.toFloat(),
+            mPaint
+        )
+
+
+
         canvas?.apply {
-            save()
-            canvas.drawRect(Rect(100, 100, 500, 500), mPaint)
-            mPaint.color = 0x6600ff99
-            mPaint.xfermode = PorterDuffXfermode(PorterDuff.Mode.DARKEN)  //设置图层混合模式
-            canvas.drawBitmap(decodeResource, 100F, 110F, mPaint)
-            mPaint.xfermode = null   // 用完及时清除 Xfermode
+//            save()
+            //背景色设为白色，方便比较效果
+            canvas.drawColor(Color.WHITE)
+            var newPaint = Paint()
+            canvas.drawBitmap(destBitmap, null, Rect(0, 0, width, height), newPaint)
+            newPaint.xfermode = PorterDuffXfermode(PorterDuff.Mode.SRC_IN)  //设置图层混合模式
+            canvas.drawBitmap(srcBitmap, null, Rect(0, 0, width, height), newPaint)
+            newPaint.xfermode = null   // 用完及时清除 Xfermode
+//            restore()
         }
+    }
+
+
+    override fun onAttachedToWindow() {
+        super.onAttachedToWindow()
+    }
+
+    override fun onDetachedFromWindow() {
+        super.onDetachedFromWindow()
     }
 
 
