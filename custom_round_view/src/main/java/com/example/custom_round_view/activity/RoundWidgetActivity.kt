c80c42213198242f6ed133771f86215d12a8fc55
zhoubencheng <zhoubencheng@xiaomi.com>
2021-11-30 21:23:10 +0800

    新增PorterDuffXfermode绘制
    Signed-off-by: zhoubencheng <zhoubencheng@xiaomi.com>

--- custom_round_view/src/main/java/com/example/custom_round_view/activity/RoundWidgetActivity.kt
+++ custom_round_view/src/main/java/com/example/custom_round_view/activity/RoundWidgetActivity.kt
@@ -3,8 +3,10 @@ package com.example.custom_round_view.activity
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.alibaba.android.arouter.facade.annotation.Route
+import com.bumptech.glide.Glide
 import com.example.custom_round_view.R
 import com.example.custom_round_view.constant.MOUDLE_AROUTER
+import kotlinx.android.synthetic.main.activity_round_widget.*
 import kotlinx.android.synthetic.main.common_top_title.*
 
 @Route(path = MOUDLE_AROUTER.ROUND_WIDGET_ACTIVITY)
@@ -13,6 +15,10 @@ class RoundWidgetActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_round_widget)
         iv_back_view.setOnClickListener {  finish() }
+
+        Glide.with(this).load("http://minio.898311.xyz:8900/blogimg/16382723810737.png").into(iv_proter_duff_mode)
+        Glide.with(this).load("http://minio.898311.xyz:8900/blogimg/16382750066097.gif").into(iv_proter_duff_mode_2)
+        Glide.with(this).load("http://minio.898311.xyz:8900/blogimg/16382751465269.gif").into(iv_proter_duff_mode_3)
 //        System.out.println(MyClass().demoPrint())
 //        System.out.println(ObjectSizeFetcher.getObjectSize("size = " + MyClass()))
     }
--- custom_round_view/src/main/java/com/example/custom_round_view/widget/ProterDuffModeRoundImageView.kt
+++ custom_round_view/src/main/java/com/example/custom_round_view/widget/ProterDuffModeRoundImageView.kt
@@ -2,10 +2,12 @@ package com.example.custom_round_view.widget
 
 import android.content.Context
 import android.graphics.*
+import android.os.Handler
+import android.os.Message
 import android.util.AttributeSet
 import android.view.View
 import androidx.annotation.Nullable
-import com.example.custom_round_view.R
+import kotlinx.coroutines.*
 
 /**
  * 文章参考 https://www.jianshu.com/p/db401403385b
@@ -23,6 +25,29 @@ class ProterDuffModeRoundImageView(
         @Nullable attrs: AttributeSet?
     ) : this(context, attrs, 0)
 
+    var drawCode = 0
+
+    init {
+        //禁止硬件加速
+        setLayerType(View.LAYER_TYPE_SOFTWARE, null)
+        GlobalScope.launch(Dispatchers.Main) {
+            while (true) {
+                val result = withContext(Dispatchers.IO) {
+                    // 网络请求，并return请求结果
+                    delay(2000)
+                }
+                // 更新UI
+                invalidate()
+                drawCode++
+                if (drawCode == 3) {
+                    drawCode = 0
+                }
+            }
+        }
+
+
+    }
+
     constructor(context: Context?) : this(context, null, 0)
 
     val mPaint by lazy {
@@ -31,47 +56,118 @@ class ProterDuffModeRoundImageView(
         mPaint
     }
 
+
+    val srcBitmap by lazy {
+        mPaint.color = Color.parseColor("#ffE64A19")
+        mPaint.isAntiAlias = true                       //设置画笔为无锯齿
+        mPaint.setStrokeWidth(15f);//设置画笔粗细
+        mPaint.style = Paint.Style.FILL                 //实心矩形框
+        val bitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888)
+        val paint = Paint()
+        paint.color = Color.parseColor("#ffE64A19")
+        Canvas(bitmap).drawRoundRect(
+            RectF(
+                0f,
+                0f,
+                bitmap.width.toFloat(),
+                bitmap.height.toFloat()
+            ), 60f, 60f, paint
+        )
+        bitmap
+    }
+    val destBitmap by lazy {
+        val bitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888)
+        val paint = Paint()
+
+        paint.color = Color.parseColor("#ff1E88E5")
+        Canvas(bitmap).drawRect(
+            0f,
+            0f,
+            bitmap.width.toFloat(),
+            bitmap.height.toFloat(),
+            paint
+        )
+        bitmap
+    }
+
     //https://www.jianshu.com/p/d11892bbe055
     override fun draw(canvas: Canvas?) {
         super.draw(canvas)
-        val srcBitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888)
-        val destBitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888)
-//        createBitmap.eraseColor(0)
-        mPaint.color = Color.parseColor("#ffE64A19")
-//        mPaint.isAntiAlias = true                       //设置画笔为无锯齿
-        mPaint.setStrokeWidth(15f);//设置画笔粗细
-        mPaint.style = Paint.Style.FILL                 //实心矩形框
-
-        Canvas(srcBitmap).drawRoundRect(
-            RectF(
-                50f,
-                50f,
-                srcBitmap.width.toFloat(),
-                srcBitmap.height.toFloat()
-            ), 30f, 30f, mPaint
-        )
-
-        mPaint.color = Color.parseColor("#ff1E88E5")
-        Canvas(destBitmap).drawRect(
-            0f,
-            0f,
-            destBitmap.width.toFloat(),
-            destBitmap.height.toFloat(),
-            mPaint
-        )
+        when (drawCode) {
+            0 -> {
+                drawSrcOlny(canvas)
+            }
+            1 -> {
+                drawDestOlny(canvas)
+            }
+            2 -> {
+                drawMixOlny(canvas)
+            }
+        }
+    }
 
 
+    fun drawSrcOlny(canvas: Canvas?) {
 
         canvas?.apply {
-//            save()
+            save()
+            //背景色设为白色，方便比较效果
+            canvas.drawColor(Color.WHITE)
+            var newPaint = Paint()
+            canvas.drawBitmap(srcBitmap, null, Rect(0, 0, width, height), newPaint)
+            newPaint.xfermode = null   // 用完及时清除 Xfermode
+            newPaint.color = Color.parseColor("#212121")
+            newPaint.textSize = 42f
+            newPaint.strokeWidth = 3f
+            newPaint.style = Paint.Style.STROKE
+            newPaint.isAntiAlias = true
+            canvas.drawText("绘制src", 60f, (height / 2 + 21f), newPaint)
+            restore()
+        }
+    }
+
+    fun drawDestOlny(canvas: Canvas?) {
+
+        canvas?.apply {
+            save()
             //背景色设为白色，方便比较效果
             canvas.drawColor(Color.WHITE)
             var newPaint = Paint()
             canvas.drawBitmap(destBitmap, null, Rect(0, 0, width, height), newPaint)
-            newPaint.xfermode = PorterDuffXfermode(PorterDuff.Mode.SRC_IN)  //设置图层混合模式
-            canvas.drawBitmap(srcBitmap, null, Rect(0, 0, width, height), newPaint)
             newPaint.xfermode = null   // 用完及时清除 Xfermode
-//            restore()
+            newPaint.color = Color.parseColor("#212121")
+            newPaint.textSize = 42f
+            newPaint.strokeWidth = 3f
+            newPaint.style = Paint.Style.STROKE
+            newPaint.isAntiAlias = true
+            canvas.drawText("绘制dest", 60f, (height / 2 + 21f), newPaint)
+            restore()
+        }
+    }
+
+    fun drawMixOlny(canvas: Canvas?) {
+
+        canvas?.apply {
+            save()
+            //背景色设为白色，方便比较效果
+            canvas.drawColor(Color.WHITE)
+
+            canvas.drawBitmap(destBitmap, null, Rect(0, 0, width, height), mPaint)
+            mPaint.xfermode = PorterDuffXfermode(PorterDuff.Mode.DST_IN)  //设置图层混合模式
+            canvas.drawBitmap(srcBitmap, null, Rect(0, 0, width, height), mPaint)
+            mPaint.xfermode = null   // 用完及时清除 Xfermode
+            mPaint.color = Color.parseColor("#212121")
+            mPaint.textSize = 42f
+            mPaint.strokeWidth = 3f
+            mPaint.style = Paint.Style.STROKE
+            mPaint.isAntiAlias = true
+            canvas.drawText(
+                "使用xfermode的形式绘制圆角图形,最终剩余的图形部分是src部分",
+                60f,
+                (height / 2 + 21f),
+                mPaint
+            )
+            restore()
         }
     }
 
